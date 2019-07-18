076683ef092c5ec53901bcbcea9d6fe3673cae36
a001564 <a001564@pc-001564-1.local>
2019-07-18 14:44:30 +0900

    added cipher_b(), cipher without polynomial calculations

--- cipher.c
+++ cipher.c
@@ -31,3 +31,23 @@ void cipher(const uint8_t *in, uint8_t *out, const struct expKey *ekey){
   }
   //free
 }
+void cipher_b(const uint8_t *in, uint8_t *out, const struct expKey *ekey){
+  ClearWord(out);
+  int in_count=0, out_count=0;
+  struct state s;
+  clearState(&s);
+  for(int i=0; i<4; i++){
+    for(int k=0; k<4; k++) setState(k,i,in[in_count++],&s);
+  }
+  AddRoundKey_generic(&s,ekey,0);
+  for(int i=1; i<11; i++){
+    SubState_b(&s,&s);
+    ShiftRows(&s);
+    if(i!=10)MixColumns_b(&s);
+    AddRoundKey_generic(&s,ekey,i*4);
+  }
+  for(int i=0; i<4; i++){
+    for(int k=0; k<4; k++) out[out_count++] = s.block[k][i];
+  }
+
+}
--- cipher.h
+++ cipher.h
@@ -1,3 +1,4 @@
 #pragma once
 // inputs/outputs message as 4*4 bytes, input expkey(already expanded)
 void cipher(const uint8_t *in, uint8_t *out, const struct expKey *ekey);
+void cipher_b(const uint8_t *in, uint8_t *out, const struct expKey *ekey);
